f9dc65154e21b9c11d2aa9e903f2f751b71b366d
cotukob-code <cotukob@gmail.com>
2026-05-18 19:51:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
+const express = require("express");
+const http = require("http");
 const WebSocket = require("ws");
 
-const PORT = process.env.PORT || 8080;
-const wss = new WebSocket.Server({ port: PORT });
+const PORT = process.env.PORT || 10000;
+
+const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ noServer: true });
 
 wss.on("connection", ws => {
-  console.log("Client connected");
+    console.log("Client connected");
 
-  ws.on("message", msg => {
-    wss.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(msg);
-      }
+    ws.on("message", msg => {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(msg.toString());
+            }
+        });
     });
-  });
 
-  ws.on("close", () => console.log("Client disconnected"));
+    ws.on("close", () => console.log("Client disconnected"));
 });
 
-console.log("WS running on port", PORT);
+server.on("upgrade", (req, socket, head) => {
+    wss.handleUpgrade(req, socket, head, ws => {
+        wss.emit("connection", ws, req);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("WebSocket server is running");
+});
+
+server.listen(PORT, () => {
+    console.log("WS running on port", PORT);
+});
